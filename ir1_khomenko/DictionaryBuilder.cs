02fff3edcf523e44f92340a41173dd8e07c1af22
M. K. <42112702+Emril44@users.noreply.github.com>
2024-02-07 18:30:25 +0200

    Implement inverted index

--- ir1_khomenko/DictionaryBuilder.cs
+++ ir1_khomenko/DictionaryBuilder.cs
@@ -10,26 +10,64 @@ namespace ir1_khomenko
 	{
 		public Dictionary<string, int> BuildDictionary(List<string> allText)
 		{
-			Dictionary<string, int> wordCount = new Dictionary<string, int>();
+			Dictionary<string, int> wordCount = new();
+			
+			Dictionary<string, HashSet<int>> invertedIndex = new();
 
-			foreach (string text in allText)
+			List<string> termsList = new();
+
+			for (int i = 0; i < allText.Count; i++)
 			{
+				string text = allText[i];
 				string[] words = text.Split(new char[] {' ', '\n', '\r', '\t', '.', ',', ';', ':', '—', '-', '(', ')', '[', ']', '{', '}', '<', '>', '\"', '\'', '\\', '/', '!', '?', '|', '_', '+', '=', '*', '&', '%', '$', '#', '@', '^', '~', '`', '“', '"', '”'}, StringSplitOptions.RemoveEmptyEntries);
 
 				foreach (string word in words)
 				{
 					string cleanWord = word.ToLower();
 
-					if (wordCount.ContainsKey(cleanWord))
+					if (wordCount.TryGetValue(cleanWord, out int value))
 					{
-						wordCount[cleanWord]++;
+						wordCount[cleanWord] = ++value;
 					}
 					else
 					{
 						wordCount[cleanWord] = 1;
+						termsList.Add(cleanWord);
 					}
+
+					if(!invertedIndex.ContainsKey(cleanWord))
+					{
+						invertedIndex[cleanWord] = new HashSet<int>();
+					}
+					invertedIndex[cleanWord].Add(i);
 				}
 			}
+
+			// Test output
+			foreach (var term in invertedIndex)
+			{
+				Console.Write($"{term.Key}: ");
+				foreach (var docID in term.Value)
+				{
+					Console.Write($"{docID} ");
+				}
+				Console.WriteLine();
+			}
+
+			//bool[,] incidencematrix = new bool[alltext.count, termslist.count];
+
+			//for (int i = 0; i < alltext.count; i++)
+			//{
+			//	string text = alltext[i];
+			//	string[] words = text.split(new char[] { ' ', '\n', '\r', '\t', '.', ',', ';', ':', '—', '-', '(', ')', '[', ']', '{', '}', '<', '>', '\"', '\'', '\\', '/', '!', '?', '|', '_', '+', '=', '*', '&', '%', '$', '#', '@', '^', '~', '`', '“', '"', '”' }, stringsplitoptions.removeemptyentries);
+
+			//	foreach (string word in words)
+			//	{
+			//		string cleanword = word.tolower();
+			//		int termindex = termslist.indexof(cleanword);
+			//		incidencematrix[i, termindex] = true;
+			//	}
+			//}
 
 			return wordCount;
 		}
